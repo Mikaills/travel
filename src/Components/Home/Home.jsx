d306adc7e0e9ca043d1f75c0a621edd349ec50fb
Mikaills <hakankayaa2@gmail.com>
2024-01-19 16:42:35 +0300

    first commit

---
+++ src/Components/Home/Home.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import './home.css'
+
+// video arif ışık at assest diye bir dosya yap ve src içine koy//
+const Home = () => {
+  return (
+    <section className='home'>
+      <div className="overlay"></div>
+      
+      <video src="">
+
+      </video>
+
+    </section>
+  )
+}
+
+export default Home
